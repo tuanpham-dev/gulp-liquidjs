2a24febcf4452df7053c1d1331640fc9b0e78ad0
Tuan Pham <tuanpham.developer@gmail.com>
2020-05-04 20:13:01 +0000

    Change tab to space

--- index.js
+++ index.js
@@ -7,63 +7,63 @@ const objectAssignDeep = require('object-assign-deep')
 const PLUGIN_NAME = 'gulp-liquidjs'
 
 module.exports = (opts) => {
-	const defaults = {
-		engine: {
-			extname: '.liquid',
-		},
-		ext: '.html',
-		filters: {},
-		tags: {},
-		plugins: [],
-		data: {},
-	}
+  const defaults = {
+    engine: {
+      extname: '.liquid',
+    },
+    ext: '.html',
+    filters: {},
+    tags: {},
+    plugins: [],
+    data: {},
+  }
 
-	const options = objectAssignDeep(defaults, opts)
-	const engine = new Liquid(options.engine)
+  const options = objectAssignDeep(defaults, opts)
+  const engine = new Liquid(options.engine)
 
-	for (const filter in options.filters) {
-		if (Object.prototype.hasOwnProperty.call(options.filters, filter)) {
-			engine.registerFilter(filter, options.filters[filter])
-		}
-	}
+  for (const filter in options.filters) {
+    if (Object.prototype.hasOwnProperty.call(options.filters, filter)) {
+      engine.registerFilter(filter, options.filters[filter])
+    }
+  }
 
-	for (const tag in options.tags) {
-		if (Object.prototype.hasOwnProperty.call(options.tags, tag)) {
-			engine.registerTag(tag, options.tags[tag])
-		}
-	}
+  for (const tag in options.tags) {
+    if (Object.prototype.hasOwnProperty.call(options.tags, tag)) {
+      engine.registerTag(tag, options.tags[tag])
+    }
+  }
 
-	if (options.plugins.length) {
-		for (const plugin of options.plugins) {
-			engine.plugin(plugin)
-		}
-	}
+  if (options.plugins.length) {
+    for (const plugin of options.plugins) {
+      engine.plugin(plugin)
+    }
+  }
 
-	return through.obj((file, encoding, callback) => {
-		const f = file
+  return through.obj((file, encoding, callback) => {
+    const f = file
 
-		if (f.data) {
-			options.data = f.data
-		}
+    if (f.data) {
+      options.data = f.data
+    }
 
-		if (f.isNull()) {
-			return callback(null, f)
-		}
+    if (f.isNull()) {
+      return callback(null, f)
+    }
 
-		if (f.isStream()) {
-			return callback(new PluginError(PLUGIN_NAME, 'Streaming is not supported'))
-		}
+    if (f.isStream()) {
+      return callback(new PluginError(PLUGIN_NAME, 'Streaming is not supported'))
+    }
 
-		if (f.isBuffer()) {
-			f.path = replaceExtension(f.path, options.ext)
+    if (f.isBuffer()) {
+      f.path = replaceExtension(f.path, options.ext)
 
-			engine.parseAndRender(f.contents.toString(), options.data)
-				.then((output) => {
-					f.contents = Buffer.from(output)
-					return callback(null, f)
-				}, err => callback(new PluginError(PLUGIN_NAME, err)))
-		}
+      engine.parseAndRender(f.contents.toString(), options.data)
+        .then((output) => {
+          f.contents = Buffer.from(output)
+          return callback(null, f)
+        }, err => callback(new PluginError(PLUGIN_NAME, err)))
+    }
 
-		return null
-	})
+    return null
+  })
 }
--- test.js
+++ test.js
@@ -1,84 +1,84 @@
 const liquid = require('./index')
 const Vinyl = require('vinyl')
 const assert = require('assert')
-const {describe, it} = require('mocha')
+const { describe, it } = require('mocha')
 
 const opts = {
-	data: {name: 'tuna'},
-	filters: {
-		test: (v) => v + ' test'
-	}
+  data: { name: 'tuna' },
+  filters: {
+    test: (v) => v + ' test'
+  }
 }
 
 describe('empty', () => {
-	it('interpolate {{...}}', cb => {
-		const stream = liquid()
+  it('interpolate {{...}}', cb => {
+    const stream = liquid()
 
-		stream.on('data', file => {
-			assert.equal('hi ', file.contents.toString())
-			cb()
-		})
+    stream.on('data', file => {
+      assert.equal('hi ', file.contents.toString())
+      cb()
+    })
 
-		stream.write(new Vinyl({
-			path: 'file.liquid',
-			contents: Buffer.from('hi {{name}}')
-		}))
-	})
+    stream.write(new Vinyl({
+      path: 'file.liquid',
+      contents: Buffer.from('hi {{name}}')
+    }))
+  })
 
-	it('default tags {%...%}', cb => {
-		const stream = liquid(opts)
+  it('default tags {%...%}', cb => {
+    const stream = liquid(opts)
 
-		stream.on('data', function(file) {
-			assert.equal('OK', file.contents.toString())
-			cb()
-		})
+    stream.on('data', function (file) {
+      assert.equal('OK', file.contents.toString())
+      cb()
+    })
 
-		stream.write(new Vinyl({
-			path: 'file.liquid',
-			contents: Buffer.from('{% if name %}OK{% endif %}')
-		}))
-	})
+    stream.write(new Vinyl({
+      path: 'file.liquid',
+      contents: Buffer.from('{% if name %}OK{% endif %}')
+    }))
+  })
 
-	it('default filter {{... | ...}}', cb => {
-		const stream = liquid(opts)
-		stream.on('data', file => {
-			assert.equal('nana', file.contents.toString())
-			cb()
-		})
+  it('default filter {{... | ...}}', cb => {
+    const stream = liquid(opts)
+    stream.on('data', file => {
+      assert.equal('nana', file.contents.toString())
+      cb()
+    })
 
-		stream.write(new Vinyl({
-			path: 'file.liquid',
-			contents: Buffer.from('{{name | replace: "tu", "na"}}')
-		}))
-	})
+    stream.write(new Vinyl({
+      path: 'file.liquid',
+      contents: Buffer.from('{{name | replace: "tu", "na"}}')
+    }))
+  })
 })
 
 describe('with options', () => {
-	it('interpolate {{...}}', cb => {
-		const stream = liquid(opts)
+  it('interpolate {{...}}', cb => {
+    const stream = liquid(opts)
 
-		stream.on('data', file => {
-			assert.equal('hi tuna', file.contents.toString())
-			cb()
-		})
+    stream.on('data', file => {
+      assert.equal('hi tuna', file.contents.toString())
+      cb()
+    })
 
-		stream.write(new Vinyl({
-			path: 'file.liquid',
-			contents: Buffer.from('hi {{name}}')
-		}))
-	})
+    stream.write(new Vinyl({
+      path: 'file.liquid',
+      contents: Buffer.from('hi {{name}}')
+    }))
+  })
 
-	it('custom filters {{... | ...}}', cb => {
-		const stream = liquid(opts)
+  it('custom filters {{... | ...}}', cb => {
+    const stream = liquid(opts)
 
-		stream.on('data', file => {
-			assert.equal('tuna test', file.contents.toString())
-			cb()
-		})
+    stream.on('data', file => {
+      assert.equal('tuna test', file.contents.toString())
+      cb()
+    })
 
-		stream.write(new Vinyl({
-			path: 'file.liquid',
-			contents: Buffer.from('{{name | test}}')
-		}))
-	})
+    stream.write(new Vinyl({
+      path: 'file.liquid',
+      contents: Buffer.from('{{name | test}}')
+    }))
+  })
 })
